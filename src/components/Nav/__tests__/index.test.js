3dad7fa7ebe1651f72a49e637eb75da49c336a51
Marcia Skousen <75454722+marcysko@users.noreply.github.com>
2021-06-08 13:19:28 -0700

    add gallery component

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -13,24 +13,24 @@ describe('Nav component', () => {
 
   it('matches snapshot', () => {
     const { asFragment } = render(<Nav />);
-    
+
     expect(asFragment()).toMatchSnapshot();
   });
 })
 
 describe('emoji is visible', () => {
-    it('inserts emoji into the h2', () => {
+  it('inserts emoji into the h2', () => {
     const { getByLabelText } = render(<Nav />);
-  
-    expect(getByLabelText('camera')).toHaveTextContent('📸');
-    });
-  })  
+
+    expect(getByLabelText('camera')).toHaveTextContent('ðŸ“¸');
+  });
+})
 
 describe('links are visible', () => {
   it('inserts text into the links', () => {
     const { getByTestId } = render(<Nav />);
 
-    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    expect(getByTestId('link')).toHaveTextContent('!');
     expect(getByTestId('about')).toHaveTextContent('About me');
   });
 
