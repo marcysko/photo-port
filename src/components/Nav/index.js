2d2fb30d45a412fdd6bf3ee6302c8f7ba8184051
Marcia Skousen <75454722+marcysko@users.noreply.github.com>
2021-06-09 13:07:26 -0700

    update testing

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -14,7 +14,7 @@ function Nav(props) {
     <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
-          <span role="img" aria-label="camera"> ðŸ“¸</span> Oh Snap!
+          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
       </h2>
       <nav>
