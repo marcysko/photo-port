7f5c74c3deea1cddad90e3056c0b6b03a39c393c
Marcia Skousen <75454722+marcysko@users.noreply.github.com>
2021-06-08 21:39:10 -0700

    add contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactForm() {
+    
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+      }
+    
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+      }
+      
+      
     return (
         <section>
           <h1>Contact me</h1>
-          <form id="contact-form">
+          <form id="contact-form" onSubmit={handleSubmit}>
             <div>
               <label htmlFor="name">Name:</label>
-              <input type="text" name="name" />
+              <input type="text" defaultValue={name} onChange={handleChange} name="name" />
             </div>
             <div>
               <label htmlFor="email">Email address:</label>
-              <input type="email" name="email" />
+              <input type="email" defaultValue={email} name="email" onChange={handleChange} />
             </div>
             <div>
               <label htmlFor="message">Message:</label>
-              <textarea name="message" rows="5" />
+              <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
             </div>
             <button type="submit">Submit</button>
           </form>
